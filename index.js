6f3e0261bc865b2bcb83f6840fc12b2c60608ced
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-10-02 18:19:44 +0600

    post data to DB form client site

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgaci.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
-app.use(bodyParser)
+
+app.use(bodyParser.json())
+app.use(cors());
 const port = 5000
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
 client.connect(err => {
-  const products = client.db("Ema-Ecommerce-Store").collection("products");
+  const productsCollection = client.db("Ema-Ecommerce-Store").collection("products");
   // console.log('database connected')
   app.post('/addProduct', (req, res) => {
-    const product = req.body;
-    products.insertOne(product)
+    const products = req.body;
+    productsCollection.insertMany(products)
     .then(result=> {
       console.log(result);
+      res.send(result.insertedCount)
     })
   })
 
